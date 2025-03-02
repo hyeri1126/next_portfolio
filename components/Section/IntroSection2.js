fbecc90cb6efc911219aeaf1cf71c1515d14c568
hyeri1126 <hyeri1126@ewhain.net>
2025-03-02 21:35:25 +0900

    intro backgrounimage 변경

--- components/Section/IntroSection2.js
+++ components/Section/IntroSection2.js
@@ -120,12 +120,15 @@ export default function IntroSection2(){
                     {isMobile ? (
                       <div
                         style={{
-                          backgroundImage: `/images/black1.jpg`,
+                          background: "url('/images/black1.jpg')",
                           backgroundSize: 'cover',
                           backgroundPosition: 'center',
                           width: '100%',
                           height: '100%',
-                          position: 'absolute'
+                          position: 'absolute',
+                          top: 0,
+                          left: 0,
+                          zIndex: 2  
                         }}
                       >
                       </div>
